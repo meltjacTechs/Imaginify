918b5c2e4fe4dc8879719ad7ca5d92e4a5a4709d
meltjacTechs <ebwire02@gmail.com>
2024-02-17 01:51:21 +0300

    Update mongoose.ts

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -3,7 +3,7 @@ import mongoose, {Mongoose} from "mongoose";
 const MONGODB_URL = process.env.MONGODB_URL;
 // In Express Js  u connect to MongoDB once but in Next Js we call it on each and every server action.
 // Thats because Next Js runs in a serverless environment, Server less Functions are stateless hence startup
-//  to handle a request and shut down right after, without mainting a continous connection to database
+//  to handle a request and shut down right after, without maintaining a continous connection to database
 interface MongooseConnection {
     conn: Mongoose | null;
     promise: Promise<Mongoose> | null;
