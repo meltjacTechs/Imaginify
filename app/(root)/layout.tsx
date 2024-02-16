3beb9686a97cd4c19a83fc429104cc63c9bff8f3
meltjacTechs <ebwire02@gmail.com>
2024-02-17 00:51:46 +0300

    Mobile Nav UI DevOps

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,11 @@
+import MobileNav from "@/components/shared/MobileNav";
 import Sidebar from "@/components/shared/Sidebar";
 
 const Layout = ({children}: {children: React.ReactNode}) => {
   return (
     <main className="root">
       <Sidebar />
+      <MobileNav />
         <div className="root-container">
             <div className="wrapper">
                {children}
--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,10 +1,34 @@
-
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+  
 
 const MobileNav = () => {
   return (
-    <div>
-      
-    </div>
+   <header className="header">
+    <Link href="/" className="flex item-center gap-2 md:py-2">
+      <Image
+        src='/assets/images/logo-text.svg'
+        alt='logo'
+        width={180}
+        height={28}
+       />
+    </Link>
+
+    <nav className="flex gap-2">
+        <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+        </SignedIn>
+    </nav>
+   </header>
   );
 }
 
