d827e0089f6e9c4ece163a5197d5322129084130
meltjacTechs <ebwire02@gmail.com>
2024-02-16 22:04:57 +0300

    Bugs and Typos

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,4 +1,4 @@
-import { SignIn } from '@clerk/clerk-react';
+import { SignIn } from '@clerk/nextjs';
 
 const SignInPage = () => {
   return <SignIn />
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,4 +1,4 @@
-import { SignUp } from '@clerk/clerk-react';
+import { SignUp } from '@clerk/nextjs';
 
 const SignUpPage = () => {
   return <SignUp />
