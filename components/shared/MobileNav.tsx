a6490f61c477040c575d416331266dbe6caf304a
meltjacTechs <ebwire02@gmail.com>
2024-02-17 01:15:20 +0300

    Update MobileNav.tsx

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,17 +1,15 @@
-import {
-    Sheet,
-    SheetContent,
-    SheetDescription,
-    SheetHeader,
-    SheetTitle,
-    SheetTrigger,
-  } from "@/components/ui/sheet"
+"use client"
+
+import { Sheet, SheetContent, SheetTrigger, } from "@/components/ui/sheet"
+import { navLinks } from "@/constants";
 import { SignedIn, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
   
 
 const MobileNav = () => {
+    const pathname  = usePathname();
   return (
    <header className="header">
     <Link href="/" className="flex item-center gap-2 md:py-2">
@@ -26,6 +24,55 @@ const MobileNav = () => {
     <nav className="flex gap-2">
         <SignedIn>
             <UserButton afterSignOutUrl="/" />
+
+            <Sheet>
+             <SheetTrigger>
+                <Image 
+                    src="/assets/icons/menu.svg"
+                    alt="menu"
+                    width={32}
+                    height={32}
+                    className="cursor-pointer"
+                />
+             </SheetTrigger>
+              <SheetContent className="sheet-content sm:w-64">
+                <>
+                  <Image
+                    src="/assets/images/logo-text.svg"
+                    alt="logo"
+                    width={152}
+                    height={23}
+                    className="cursor-pointer" 
+                  />
+
+                   <ul className="header-nav_elements">
+                    {navLinks.map((link) => {
+                        const isActive = link.route === pathname
+                        return (
+                            <li 
+                              className={`${isActive && 
+                                 'gradient-text'} p=18 flex
+                                 whitespace-nowrap text-dark-700`}
+                              key={link.route} 
+                            >
+                                <Link className="sidebar-link" href={link.route}>
+                                    <Image 
+                                       src={link.icon}
+                                       alt="logo"
+                                       width={24}
+                                       height={24}
+                                       className={`${isActive && 'brightness-200'}`}
+                                    />
+                                    {link.label}
+                                </Link>
+                            </li>
+                        )
+                    })}
+                    </ul>
+                </>
+             </SheetContent>
+            </Sheet>
+
         </SignedIn>
     </nav>
    </header>
