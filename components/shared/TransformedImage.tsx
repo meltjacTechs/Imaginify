658d401f3ba49c70fb4aa8e1edd1917c87f272cb
meltjacTechs <ebwire02@gmail.com>
2024-02-18 22:29:22 +0300

    Update TransformedImage.tsx

--- components/shared/TransformedImage.tsx
+++ components/shared/TransformedImage.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const TransformedImage = () => {
+const TransformedImage = ({ image, type, title, 
+    transformationConfig, isTransforming, setIsTransforming,
+hasDownload = false}: TransformedImageProps) => {
   return (
-    <div>
-      TransformationImage
+    <div className="flex flex-col gap-4">
+      <div className="flex-between">
+        <h3 className="h3-bold text-dark-600">Transformed</h3>
+      </div>
     </div>
   );
 }
